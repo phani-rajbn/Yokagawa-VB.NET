4bf5398355d2f1d6f92d38e49a058530d88ed5b9
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2021-12-11 10:22:41 +0530

    Add files via upload

---
+++ SampleWinAppDay7/Form1.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmUserInfo
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtBirthDate = New System.Windows.Forms.TextBox()
+        Me.cmbQualification = New System.Windows.Forms.ComboBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Tai Le", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(259, 26)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(422, 48)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "User Registration Form"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(210, 85)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(111, 26)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Full Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(168, 142)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(153, 26)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Postal Address:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(186, 301)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(135, 26)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Date of Birth:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(186, 358)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(135, 26)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Qualification:"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(327, 82)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(408, 34)
+        Me.txtName.TabIndex = 5
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(327, 142)
+        Me.txtAddress.Multiline = True
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(408, 128)
+        Me.txtAddress.TabIndex = 6
+        '
+        'txtBirthDate
+        '
+        Me.txtBirthDate.Location = New System.Drawing.Point(327, 293)
+        Me.txtBirthDate.Name = "txtBirthDate"
+        Me.txtBirthDate.Size = New System.Drawing.Size(408, 34)
+        Me.txtBirthDate.TabIndex = 7
+        '
+        'cmbQualification
+        '
+        Me.cmbQualification.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbQualification.FormattingEnabled = True
+        Me.cmbQualification.Items.AddRange(New Object() {"Graduation", "Post Graduation", "Under Graduation", "P.hd."})
+        Me.cmbQualification.Location = New System.Drawing.Point(328, 355)
+        Me.cmbQualification.Name = "cmbQualification"
+        Me.cmbQualification.Size = New System.Drawing.Size(407, 34)
+        Me.cmbQualification.TabIndex = 8
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(267, 425)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(188, 54)
+        Me.btnSave.TabIndex = 9
+        Me.btnSave.Text = "&Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancel.Location = New System.Drawing.Point(572, 425)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(188, 54)
+        Me.btnCancel.TabIndex = 10
+        Me.btnCancel.Text = "&Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'frmUserInfo
+        '
+        Me.AcceptButton = Me.btnSave
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 26.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.CancelButton = Me.btnCancel
+        Me.ClientSize = New System.Drawing.Size(939, 560)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.cmbQualification)
+        Me.Controls.Add(Me.txtBirthDate)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Font = New System.Drawing.Font("Microsoft Tai Le", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.Maroon
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmUserInfo"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "User Info Details"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents txtBirthDate As TextBox
+    Friend WithEvents cmbQualification As ComboBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnCancel As Button
+End Class
