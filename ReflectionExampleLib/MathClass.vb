7707e26f54c6bd4189a3296eab0b0fe8edbd166d
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2022-01-01 15:36:02 +0530

    Add files via upload

---
+++ ReflectionExampleLib/MathClass.vb
@@ -0,0 +1,14 @@
+﻿Public Class MathClass
+    Public Function AddFunc(firstVal As Double, secondval As Double) As Double
+        Return firstVal + secondval
+    End Function
+    Public Function SubFunc(firstVal As Double, secondval As Double) As Double
+        Return firstVal - secondval
+    End Function
+    Public Function MulFunc(firstVal As Double, secondval As Double) As Double
+        Return firstVal * secondval
+    End Function
+    Public Function DivFunc(firstVal As Double, secondval As Double) As Double
+        Return firstVal / secondval
+    End Function
+End Class
